0ec733bb2d65d47f0406408cae33e98883d9388b
MAFERCARRE <mafercarrenio@gmail.com>
2026-09-13 23:02:29 -0300

    cree Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+export function Navbar() {
+    return (
+        <header >
+            <h1>
+                Recetas Sin TACC
+            </h1>
+
+            <button>
+                Mi Lista
+            </button>
+        </header>
+    );
+}
